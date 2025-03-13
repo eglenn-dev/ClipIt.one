be99b36ab35ecc9ef835aada8c1011ff94b22347
Ethan Glenn <ethan+git@eglenn.dev>
2025-03-13 15:01:10 -0600

    feat: Implement session check and redirect to login in HomeLayout

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,15 +1,19 @@
 import HomeHeader from "@/components/home-header";
 import SidePanel from "@/components/side-panel";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
 export const metadata = {
     title: "Dashboard | ClipIt.one",
 };
 
-export default function HomeLayout({
+export default async function HomeLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const session = await getSession();
+    if (!session) redirect("/login");
     return (
         <div className="flex min-h-screen flex-col">
             <HomeHeader />
